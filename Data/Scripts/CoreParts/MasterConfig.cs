23f8f28a3e0ea92529f677b7f3737585e2c8ef61
Nevada Reno <mr.nevadareno@gmail.com>
2023-04-08 12:11:21 -0700

    init

---
+++ Data/Scripts/CoreParts/MasterConfig.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Scripts
+{
+    partial class Parts
+    {
+        internal Parts()
+        {
+            // naming convention: WeaponDefinition Name
+            //
+            // Enable your definitions using the follow syntax:
+            // PartDefinitions(Your1stDefinition, Your2ndDefinition, Your3rdDefinition);
+            // PartDefinitions includes both weapons and phantoms
+            PartDefinitions(ProtonBeam, PhysBlink, Cannon105mm, Cannon250mm, Cannon500mm, Cannon750mm,
+                Cannon1000mm, Cannon1500mm, Cannon2000mm, Cannon2500mm);
+
+
+            //ArmorDefinitions(Armor1, Armor2);
+            //SupportDefinitions(ArmorEnhancer1A);
+            //UpgradeDefinitions(Upgrade75a, Upgrade75b);
+        }
+    }
+}
