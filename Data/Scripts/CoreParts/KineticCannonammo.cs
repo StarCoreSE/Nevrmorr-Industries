c4b4ed3efc0c48f9e4166ce7439640461821db1a
Nevada Reno <mr.nevadareno@gmail.com>
2023-04-13 13:10:05 -0700

    made new particle bigger

--- Data/Scripts/CoreParts/KineticCannonammo.cs
+++ Data/Scripts/CoreParts/KineticCannonammo.cs
@@ -207,7 +207,7 @@ namespace Scripts
                     MinArmingTime = 0, // In ticks, before the Ammo is allowed to explode, detonate or similar; This affects shrapnel spawning.
                     NoVisuals = false,
                     NoSound = false,
-                    ParticleScale = 1,
+                    ParticleScale = 5,
                     CustomParticle = "Energy_Explosion", // Particle SubtypeID, from your Particle SBC
                     CustomSound = "soundName", // SubtypeID from your Audio SBC, not a filename
                     Shape = Diamond, // Round or Diamond shape.  Diamond is more performance friendly.
